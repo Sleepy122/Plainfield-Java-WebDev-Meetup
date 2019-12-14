8daf7e4433e045749676a0e25e2e17f9937987fa
unknown <calder.jones3155@gmail.com>
2019-12-14 12:25:38 -0600

    first commit !

---
+++ SpringMvcTemplate/src/main/java/com/webdev/SpringMvcTemplate/SpringMvcTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.webdev.SpringMvcTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcTemplateApplication.class, args);
+	}
+
+}
---
+++ SpringMvcTemplate/src/test/java/com/webdev/SpringMvcTemplate/SpringMvcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.SpringMvcTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
